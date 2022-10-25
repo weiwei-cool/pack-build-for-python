5bf5d92ed6b4c670d0bbc532c04ca7331985d258
weiwei <2769694799@qq.com>
2022-10-25 21:21:53 +0800

    实现创建文件夹1

--- main.py
+++ main.py
@@ -1,16 +1,21 @@
-# 这是一个示例 Python 脚本。
-
-# 按 Shift+F10 执行或将其替换为您的代码。
-# 按 双击 Shift 在所有地方搜索类、文件、工具窗口、操作和设置。
-
-
-def print_hi(name):
-    # 在下面的代码行中使用断点来调试脚本。
-    print(f'Hi, {name}')  # 按 Ctrl+F8 切换断点。
+import sys
+import os
+import json
+import requests
 
 
-# 按间距中的绿色按钮以运行脚本。
-if __name__ == '__main__':
-    print_hi('PyCharm')
+class Main:
+    def __init__(self, name):
+        self.name = name
+        self.url = "flystudiokey.xyz"
+        if not os.path.isdir("./bin"):
+            os.mkdir("./bin")
+            self.first_run()
 
-# 访问 https://www.jetbrains.com/help/pycharm/ 获取 PyCharm 帮助
+    def file_write(self):
+        pass
+
+    def first_run(self):
+        res = json.loads(requests.get(url=f"https://ann.{self.url}/get-app-ann").text)
+        d_url = res["data"][0]["d_url"]
+        file = requests.get(d_url["7zip"])
